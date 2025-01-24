1fa800f1b74b4443aa738cf27a752a702524d296
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2025-01-24 22:26:58 +0700

    submission 1

---
+++ src/server.js
@@ -0,0 +1,76 @@
+const Hapi = require('@hapi/hapi');
+require('dotenv').config();
+
+const AlbumsService = require('./services/postgres/albumsService');
+const AlbumsValidator = require('./validator/albums');
+const albums = require('./api/albums');
+
+const SongsService = require('./services/postgres/songsService');
+const SongsValidator = require('./validator/songs');
+const songs = require('./api/songs');
+
+const init = async () => {
+    const albumsService = new AlbumsService();
+    const songsService = new SongsService();
+
+    const server = Hapi.server({
+        port: process.env.PORT || 5000,
+        host: process.env.HOST || 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    await server.register([
+        {
+            plugin: albums,
+            options: {
+                service: albumsService,
+                validator: AlbumsValidator,
+            },
+        },
+        {
+            plugin: songs,
+            options: {
+                service: songsService,
+                validator: SongsValidator,
+            },
+        },
+    ]);
+
+    server.ext('onPreResponse', (request, h) => {
+        const { response } = request;
+
+        if (response instanceof Error) {
+            if (response instanceof ClientError) {
+                const newResponse = h.response({
+                    status: 'fail',
+                    message: response.message,
+                });
+                newResponse.code(response.statusCode);
+                return newResponse;
+            }
+
+            if (!response.isServer) {
+                return h.continue;
+            }
+
+            const newResponse = h.response({
+                status: 'error',
+                message: 'terjadi kegagalan pada server kami',
+            });
+            console.log(`Error: ${response.message}`);
+            newResponse.code(500);
+            return newResponse;
+        }
+
+        return h.continue;
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
